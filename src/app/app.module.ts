743efffdc8eb567ba55b682a4ef4c1865cfe9be7
Diokou_Tech <diokoucheikh@gmail.com>
2022-04-21 02:08:35 +0000

    update add icons bootsrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsComponent } from './components/news/news.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+const appRoutes : Routes = [
+  {
+    path: '', component: NewsComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NewsComponent
+    NewsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,15 +1,25 @@
+import { DataService } from './../../service/data.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
-
-  constructor() { }
+  news:any;
+  response:any;
+  constructor(private dataService:DataService) { }
 
   ngOnInit(): void {
+    this.getDataNews(1);
   }
 
+  getDataNews(page:any){
+    console.log('recuperation des données ZOLA');
+    this.dataService.getData(page).subscribe(res =>{
+      console.log('recupeartion données',res);
+      this.response = res;
+      this.news = this.response.data;
+    });
+  }
 }
